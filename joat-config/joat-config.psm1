e4c4da6dae1c3c680b187961bdd9d5d46ab4d11c
Jim Wallace <seekatar@gmail.com>
2018-11-16 15:17:14 -0500

    move joat-config to its own repo

--- joat-config/joat-config.psm1
+++
@@ -1,7 +0,0 @@
-
-foreach( $i in (Get-ChildItem (Join-Path $PSScriptRoot "*.ps1") -File -Exclude *.tests.ps1 -Recurse) )
-{
-    . $i
-}
-
-Export-ModuleMember -Function "Find-ConfigData","Get-ConfigData","Set-ConfigData","Remove-ConfigData" -Alias *
